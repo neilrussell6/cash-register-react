4ec1bb2cd186c63b17ca2373f666a21e61188ef3
Neil Russell <neilrussell6@gmail.com>
2018-08-14 09:54:11 +0200

    refactor: clean up reducers index.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,13 @@
 import {combineReducers} from "redux";
-import {routerReducer} from "react-router-redux";
-import {reducer as formReducer} from "redux-form";
+import {routerReducer as routing} from "react-router-redux";
+import {reducer as form} from "redux-form";
 import {inventoryItems} from "./inventory";
 import {billItems, billSummary} from "./bill";
 import {keypad} from "./keypad";
 
 export const reducers = combineReducers({
-  routing: routerReducer,
-  form: formReducer,
+  routing,
+  form,
   inventoryItems,
   billItems,
   billSummary,
