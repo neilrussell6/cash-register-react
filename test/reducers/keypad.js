2a66f593f6309691b54ab7c0442f8612681cd2dc
Neil Russell <neilrussell6@gmail.com>
2018-08-19 19:53:42 +0200

    feat: parametrize keybpad tests.

--- test/reducers/keypad.js
+++ test/reducers/keypad.js
@@ -1,4 +1,6 @@
 import {expect} from "chai";
+import {parametrize} from "js-parametrize";
+
 import {keypad, KEYPAD_DEFAULT_STATE} from "../../src/reducers/keypad";
 import {KEYPAD_UPDATE, KEYPAD_CLEAR, KEYPAD_SET_POINT} from "../../src/actions";
 
@@ -6,119 +8,91 @@ describe('keypad reducer', () => {
 
   describe('KEYPAD_UPDATE', () => {
 
-    // TODO: parametrize
-    it('should update keypad with provided value', () => {
-      const reducer = keypad[KEYPAD_UPDATE];
+    const reducer = keypad[KEYPAD_UPDATE];
 
-      // given
-      // ... keypad is currently 0.00 with no point
-      let keypad_state = {integerPart: 0, fractionalPart: 0, hasPoint: false, amount: 0};
+    parametrize([
+      [
+        {integerPart: 0, fractionalPart: 0, hasPoint: false, amount: 0},
+        4,
+        {integerPart: 4, fractionalPart: 0, hasPoint: false, amount: 400},
+      ],
+      [
+        {integerPart: 44, fractionalPart: 0, hasPoint: false, amount: 4400},
+        0,
+        {integerPart: 440, fractionalPart: 0, hasPoint: false, amount: 44000},
+      ],
+      [
+        {integerPart: 440, fractionalPart: 0, hasPoint: true, amount: 44000},
+        7,
+        {integerPart: 440, fractionalPart: 7, hasPoint: true, amount: 44070},
+      ],
+      [
+        {integerPart: 440, fractionalPart: 77, hasPoint: true, amount: 44077},
+        7,
+        {integerPart: 440, fractionalPart: 77, hasPoint: true, amount: 44077},
+      ],
+    ], (initial_state, value, expected_state) => {
+      it('should update keypad with provided value', () => {
+        // given ... keypad state is initially as provided
+        // when ... we update keypad with provided value
+        let result = reducer(initial_state, {
+          type: KEYPAD_UPDATE,
+          value,
+        });
 
-      // when ... we update keypad with 4
-      let result = reducer(keypad_state, {
-        type: KEYPAD_UPDATE,
-        value: 4,
+        // then ... should update amount as expected
+        expect(result).to.deep.equal(expected_state);
       });
-
-      // then
-      // ... should update amount as expected
-      let expected = {integerPart: 4, fractionalPart: 0, hasPoint: false, amount: 400};
-      expect(result).to.deep.equal(expected);
-
-      // given
-      // ... keypad is currently 44.00 with no point
-      keypad_state = {integerPart: 44, fractionalPart: 0, hasPoint: false, amount: 4400};
-
-      // when ... we update keypad with 0
-      result = reducer(keypad_state, {
-        type: KEYPAD_UPDATE,
-        value: 0,
-      });
-
-      // then
-      // ... should update amount as expected
-      expected = {integerPart: 440, fractionalPart: 0, hasPoint: false, amount: 44000};
-      expect(result).to.deep.equal(expected);
-
-      // given
-      // ... keypad is currently 440.00 ... with a point
-      keypad_state = {integerPart: 440, fractionalPart: 0, hasPoint: true, amount: 44000};
-
-      // when ... we update keypad with 7
-      result = reducer(keypad_state, {
-        type: KEYPAD_UPDATE,
-        value: 7,
-      });
-
-      // then
-      // ... should update amount as expected
-      expected = {integerPart: 440, fractionalPart: 7, hasPoint: true, amount: 44070};
-      expect(result).to.deep.equal(expected);
-
-      // given
-      // ... keypad is currently 440.77 with a point
-      keypad_state = {integerPart: 440, fractionalPart: 77, hasPoint: true, amount: 44077};
-
-      // when ... we update keypad with 9
-      result = reducer(keypad_state, {
-        type: KEYPAD_UPDATE,
-        value: 7,
-      });
-
-      // then
-      // ... should not update keypad
-      expected = {integerPart: 440, fractionalPart: 77, hasPoint: true, amount: 44077};
-      expect(result).to.deep.equal(expected);
     });
   });
 
   describe('KEYPAD_SET_POINT', () => {
 
-    // TODO: parametrize
-    it('should set point', () => {
-      const reducer = keypad[KEYPAD_SET_POINT];
+    const reducer = keypad[KEYPAD_SET_POINT];
 
-      // given
-      // ... keypad is currently 440.00 with no point
-      let keypad_state = {integerPart: 440, fractionalPart: 0, hasPoint: false, amount: 44000};
+    it('should set point if currently no point', () => {
+      // given ... keypad currently has no point
+      const initial_state = {integerPart: 440, fractionalPart: 0, hasPoint: false, amount: 44000};
 
       // when ... we update keypad with a point
-      let result = reducer(keypad_state, {type: KEYPAD_SET_POINT});
+      const result = reducer(initial_state, {type: KEYPAD_SET_POINT});
 
-      // then
-      // ... should update amount as expected
-      let expected = {integerPart: 440, fractionalPart: 0, hasPoint: true, amount: 44000};
+      // then ... should set point
+      const expected = {integerPart: 440, fractionalPart: 0, hasPoint: true, amount: 44000};
       expect(result).to.deep.equal(expected);
+    });
 
-      // given
-      // ... keypad is currently 440.00 with a point
-      keypad_state = {integerPart: 440, fractionalPart: 0, hasPoint: true, amount: 44000};
+    it('should not set point if currently already has point', () => {
+
+      // given ... keypad currently already has a point
+      const initial_state = {integerPart: 440, fractionalPart: 0, hasPoint: true, amount: 44000};
 
       // when ... we update keypad with a point
-      result = reducer(keypad_state, {type: KEYPAD_SET_POINT});
+      const result = reducer(initial_state, {type: KEYPAD_SET_POINT});
 
-      // then
-      // ... should not update keypad
-      expected = {integerPart: 440, fractionalPart: 0, hasPoint: true, amount: 44000};
-      expect(result).to.deep.equal(expected);
+      // then ... should not update keypad
+      expect(result).to.deep.equal(initial_state);
     });
   });
 
   describe('KEYPAD_CLEAR', () => {
 
-    // TODO: parametrize
-    it('should clear keypad', () => {
-      // given
-      // ... keypad is currently 440.77 with a point
-      const keypad_state = {integerPart: 440, fractionalPart: 77, hasPoint: true, amount: 44077};
+    const reducer = keypad[KEYPAD_CLEAR];
 
-      // when ... we clear the keypad
-      const reducer = keypad[KEYPAD_CLEAR];
-      const result = reducer(keypad_state, {type: KEYPAD_CLEAR});
+    parametrize([
+      [{integerPart: 0, fractionalPart: 0, hasPoint: false, amount: 0}],
+      [{integerPart: 440, fractionalPart: 77, hasPoint: true, amount: 44077}],
+    ], (initial_state) => {
+      it('should clear keypad', () => {
+        // given ... keypad state is initially as provided
+        const keypad_state = {integerPart: 440, fractionalPart: 77, hasPoint: true, amount: 44077};
 
-      // then
-      // ... should return keypad to it's default state
-      expect(result).to.deep.equal(KEYPAD_DEFAULT_STATE);
+        // when ... we clear the keypad
+        const result = reducer(initial_state, {type: KEYPAD_CLEAR});
+
+        // then ... should return keypad to it's default state
+        expect(result).to.deep.equal(KEYPAD_DEFAULT_STATE);
+      });
     });
   });
 });
